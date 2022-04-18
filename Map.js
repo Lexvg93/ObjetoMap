0b897d0bfe2d8f35527bf5425350885f7ceef65b
Lexvg93 <alejandro_gonzalo92@icloud.com>
2022-04-18 13:55:31 +0200

    agregamos modificiacion

--- Map.js
+++ Map.js
@@ -1,13 +1,11 @@
 /*Apuntes de curso online 
-Es un objeto que almacena pares clave-valor. Podemos utilizar objetos o valores primitivos
+Map Es un objeto que almacena pares clave-valor. Podemos utilizar objetos o valores primitivos tanto para ser usados como clave como para ser usados como valor
 
-Objetos y Maps son similares en cuanto a funcionamiento (tiene asociado claves a valores)
-
-
-
-
-
-
-
-
+Objetos y Maps son similares en cuanto a funcionamiento (tiene asociado claves a valores, se puede acceder a cada valor, eliminar claves...); pero hay diferencias
+-Las claves de un objeto son Strings y Symbols; las de un Map pueden ser de cualquier tipo
+-Para saber el tamaño de un Map utilizamos la propiedad size; en un objeto hay que hacerlo manualmente
+-Map es iterable; para iterar Object necesitamos primero las claves para iterar sobre el
+-Un Object tiene prototipo; claves por defecto en un Map pueden colisionar con las de un object
 */
+
+
