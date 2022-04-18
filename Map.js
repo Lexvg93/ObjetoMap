86c5cfcf9dea6fb6a2a217511e381aa42c6cf433
Lexvg93 <alejandro_gonzalo92@icloud.com>
2022-04-18 13:44:50 +0200

    agregamos modificiacion

---
+++ Map.js
@@ -0,0 +1,13 @@
+/*Apuntes de curso online 
+Es un objeto que almacena pares clave-valor. Podemos utilizar objetos o valores primitivos
+
+Objetos y Maps son similares en cuanto a funcionamiento (tiene asociado claves a valores)
+
+
+
+
+
+
+
+
+*/
